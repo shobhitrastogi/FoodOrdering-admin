60b336ba6968fbd6e5575978574e75c96df880fe
Shobhit Rastogi <rshobhit255@gmail.com>
2024-06-19 20:19:53 +0530

    Change in backend url

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://foodordering-backend-2y3s.onrender.com"
 
   return (
     <div>
